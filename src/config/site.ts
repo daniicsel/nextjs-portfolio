01c6b2d97cf923186ba41db3cc32acd764bc78fe
daniicsel <133073627+daniicsel@users.noreply.github.com>
2025-05-01 16:03:00 +0100

    changed name from ethan to dani

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,9 +3,9 @@ import { SidebarNavItem, SiteConfig } from "@/types";
 const site_url = process.env.NEXT_PUBLIC_APP_URL;
 
 export const siteConfig: SiteConfig = {
-    name: "Ethan Quarry",
+    name: "Daniel Eugene",
     description:
-        "Ethan Quarry - Software Engineer",
+        "Daniel Eugene - Gimp",
     url: site_url || "http://localhost:3000",
     ogImage: `${site_url}/_static/og.jpg`,
     links: {
