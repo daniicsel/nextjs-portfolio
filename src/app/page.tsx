606b496d3c8e5a9c900a8e6788953f7a76828794
daniicsel <d.eugenecollins@gmail.com>
2025-05-01 16:30:59 +0100

    change name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <main className="">
       <SectionWithOffset title="Bio">
-        <p className="mb-4 font-semibold">Hi, I'm Ethan – an eighteen-year-old software engineer based in Ireland.</p>
+        <p className="mb-4 font-semibold">Hi, I'm Daniel – an eighteen-year-old software engineer based in Ireland.</p>
         <p className="mb-4">I'm currently working with the awesome team @ <CustomLink href="https://naviro.io">Naviro</CustomLink> to build the AI agent for audiences.</p>
         <p className="mb-4">After <CustomLink href="https://hackireland.com">HackIreland</CustomLink>, I'm now working on building automated internal documentation after an engineering manager @ <CustomLink href="https://www.tines.com/blog/series-c-fundraise/">Tines</CustomLink> said they would use the product I pitched.</p>
         <p className="mb-4">But for the moment I'm really focusing on work and making sure I provide the best ROI for them hiring an 18 year old.</p>
